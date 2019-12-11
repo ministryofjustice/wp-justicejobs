21ccc3480686bbd92aae85464585d64f693ec87c
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2019-12-11 18:41:31 +0000

    Track new files

---
+++ web/app/themes/justicejobs/webpack.mix.js
@@ -0,0 +1,38 @@
+const mix = require('laravel-mix');
+const dist = 'dist/';
+
+require('laravel-mix-imagemin');
+mix.setPublicPath('./dist/');
+
+mix.js('src/js/main.js', dist + 'js/main.min.js')
+    .sass('src/scss/style.scss', dist + 'css/main.min.css')
+    .copy('src/video/*', dist + 'video')
+    .copy('src/fonts/*', dist + 'fonts')
+    .copy('node_modules/jquery/dist/jquery.min.js', dist + 'js/')
+    .copy('node_modules/rellax/rellax.min.js', dist + 'js/')
+    .copy('node_modules/slick-carousel/slick/slick.min.js', dist + 'js/')
+    .imagemin([
+            'img/**.*',
+            'img/icons/**.*'
+        ],
+        {context: './src/'},
+        {
+            optipng: {optimizationLevel: 5},
+            jpegtran: null,
+            plugins: [
+                require('imagemin-mozjpeg')({
+                    quality: 82,
+                    progressive: true,
+                })
+            ],
+        }
+    )
+    .options({
+        processCssUrls: false
+    });
+
+if (mix.inProduction()) {
+    mix.version()
+} else {
+    mix.sourceMaps()
+}
