ec792c1d88486ad39c0df72b6cf3e7e88f58900f
Robert Lowe <robert.lowe@digital.justice.gov.uk>
2022-03-09 10:57:46 +0000

    Update NPM and Hide Breadcrumb on small devices

--- web/app/themes/justicejobs/webpack.mix.js
+++ web/app/themes/justicejobs/webpack.mix.js
@@ -14,23 +14,13 @@ mix.js([
     ], dist + 'js/main.min.js')
     .js('src/js/jj-gtm.js', dist + 'js/jj-gtm.min.js')
     .sass('src/scss/style.scss', dist + 'css/main.min.css')
-    .sass('src/scss/errors/404.sass', dist + 'css/404.css')
-    .sass('src/scss/errors/error-page.sass', dist + 'css/error-page.css')
+    .sass('src/scss/errors/404.scss', dist + 'css/404.css')
+    .sass('src/scss/errors/error-page.scss', dist + 'css/error-page.css')
     .copy('src/video/*', dist + 'video')
     .copy('src/fonts/*', dist + 'fonts')
     .copy('node_modules/jquery/dist/jquery.min.js', dist + 'js/')
     .copy('node_modules/rellax/rellax.min.js', dist + 'js/')
-    .imagemin([
-            'img/**.*',
-            'img/icons/**.*'
-        ],
-        {context: './src/'},
-        {
-            optipng: {optimizationLevel: 5},
-            jpegtran: null
-        }
-    )
-    .copy('src/img/*.svg', dist + 'img')
+    .copy('src/img/', dist + 'img')
     .options({
         processCssUrls: false
     });
