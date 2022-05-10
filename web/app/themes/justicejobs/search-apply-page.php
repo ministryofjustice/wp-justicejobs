7ac0254261491b045ea9291ac5028313d288975a
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2022-05-10 16:38:57 +0100

    pound instead of #163

--- web/app/themes/justicejobs/search-apply-page.php
+++ web/app/themes/justicejobs/search-apply-page.php
@@ -226,18 +226,18 @@ Template Name: Search/Apply Template
                                     $salary_min = get_field('salary_min');
 
                                     if(!empty($salary_min)){
-                                        echo '&#163;' . number_format($salary_min);
+                                        echo '&pound;' . number_format($salary_min);
 
                                         $salary_max = get_field('salary_max');
 
                                         if(!empty($salary_max)){
 
-                                            echo ' - &#163;' . number_format($salary_max);
+                                            echo ' - &pound;' . number_format($salary_max);
 
                                         }
                                         $london_allowance = number_format(intval(get_field('salary_london')));
                                         if (!empty($london_allowance)) {
-                                            echo "<br /> + &#163;$london_allowance LWA";
+                                            echo "<br /> + &pound;$london_allowance LWA";
                                         }
                                     }
                                     ?>
