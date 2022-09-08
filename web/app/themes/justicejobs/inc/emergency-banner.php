9817bfd4c0073f984b4b99e5d3ebd599d0ae1c37
MalcolmVonMoJ <malcolm.butler@digital.justice.gov.uk>
2022-09-08 19:55:06 +0100

    URL no longer mandatory

--- web/app/themes/justicejobs/inc/emergency-banner.php
+++ web/app/themes/justicejobs/inc/emergency-banner.php
@@ -11,8 +11,10 @@
 		padding: 2rem 3rem 3rem;
 		border-bottom: 2px solid #f1f2f3;
 	">
-		<div style="		max-width:960px;
-			margin:0 auto;">
+		<div style="
+			max-width:960px;
+			margin:0 auto;"
+		>
 			
 			<div style="
 				font-weight: bold;
@@ -30,21 +32,25 @@
 				echo get_field('banner_subtext', 'option');
 			?>
 			</div>
-			<div style="
-
-			">
-				<a class="govuk-header__link emergency-banner-link" style="
-					color:#fff;
-					text-decoration:underline;
-				"
-				href="
-					<?php
-						echo get_field('banner_subtext', 'option');
-					?>
+			<?php
+				$moreinfolink = get_field('banner_link_destination', 'option');
+				if (isset($moreinfolink) && $moreinfolink != "") {
+			?>
+				<div style="
 				">
-					More information
-				</a>
-			</div>
+					<a class="govuk-header__link emergency-banner-link" style="
+						color:#fff;
+						text-decoration:underline;
+					"
+					href="
+						<?php
+							echo get_field('banner_link_destination', 'option');
+						?>
+					">
+						More information
+					</a>
+				</div>
+			<?php } ?>
 		</div>
 	</div>
 <?php } ?>
